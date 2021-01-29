3a4cc1b5188d531bc0b381096b390773fff466f4
Cuong Tran <clonebmn2itt@gmail.com>
2021-01-29 22:18:13 +0700

    fix smt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 module.exports = async (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
